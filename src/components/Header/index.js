13e78769ee6ea6af828c34f269742ed30e7a6fea
Zakamsky <alex.zakamsky@gmail.com>
2021-03-18 18:33:13 +0100

    task 2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,6 +9,8 @@ const Header = ({ title = "This is title", descr = "This is Description!", isBut
     return (
         <header className={ s.root }>
             <div className={ s.forest }></div>
+            <div className={ s.silhouette  }></div>
+            <div className={ s.moon }></div>
             <div className={ s.container }>
                 <h1>{title}</h1>
                 <p>{descr}</p>
